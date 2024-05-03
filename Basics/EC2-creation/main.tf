b76b7176dd5ee09f8f085a2bdc1cf20b7b15260a
Sneha282 <79513193+Sneha282@users.noreply.github.com>
2024-05-03 16:15:08 +0530

    Update main.tf

--- Basics/EC2-creation/main.tf
+++ Basics/EC2-creation/main.tf
@@ -1,8 +1,14 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
-}
-
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
-}
+     region = "us-east-1" 
+     access_key = "xxxxx"
+     secret_key = "xxxxxx"
+   }
+ resource "aws_instance" "example" {
+     ami           = "ami-07caf09b362be10b8"  # Specify an appropriate AMI ID
+     instance_type = "t2.micro"
+     key_name = "terrraform"
+     security_groups = "launch-wizard-4"
+     tags = {
+       Name= "Sneha-Instance"
+     }
+   }
